97ab6b551ae48f715ca9bcbd307ddcf56f6f360c
3rd <hello3rd@outlook.com>
2024-08-19 17:19:45 +0800

    feat(ipcxx.h): 提供快速引入所有功能的头文件

--- include/ipcxx/ipcxx.h
+++ include/ipcxx/ipcxx.h
@@ -1,4 +1,11 @@
 #ifndef IPCXX_H
 #define IPCXX_H
 
-#endif //IPCXX_H
+#include "ipcxx/async_queue.h"
+#include "ipcxx/event_loop.h"
+#include "ipcxx/thread_pool.h"
+#include "ipcxx/singleton.h"
+
+namespace ipcxx {}
+
+#endif // IPCXX_H
