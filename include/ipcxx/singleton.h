ba6b446c6655aed28c6798a2dffe570139443981
3rd <hello3rd@outlook.com>
2025-03-01 18:47:48 +0800

    perf(singleton): 将单例目标放入ipcxx命名空间
    将单例模板放入`ipcxx`命名空间以避免冲突
    
    添加一个简化获取单例的模板函数

--- include/ipcxx/singleton.h
+++ include/ipcxx/singleton.h
@@ -4,6 +4,7 @@
 #include <memory>
 #include <mutex>
 
+namespace ipcxx {
 /**
  * \brief 单例模板
  * \tparam T 需要作为单例模式使用的类型
@@ -18,27 +19,31 @@
  */
 template<typename T, typename... Args>
 class Singleton {
- public:
-  // 删除拷贝构造函数、移动构造函数、拷贝赋值运算符和移动赋值运算符，以防止外部复制、赋值或移动对象
-  Singleton(const Singleton &) = delete;
-  Singleton &operator=(const Singleton &) = delete;
-  Singleton(Singleton &&) = delete;
-  Singleton &operator=(Singleton &&) = delete;
+  public:
+    // 删除拷贝构造函数、移动构造函数、拷贝赋值运算符和移动赋值运算符，以防止外部复制、赋值或移动对象
+    Singleton(const Singleton &) = delete;
 
-  // 单例模板类的获取实例函数，使用完美转发和std::call_once来创建并返回单例对象
-  static T *GetInstance(Args &&... args) {
-    std::call_once(flag_, [&]() { instance_ = std::make_unique<T>(std::forward<Args>(args)...); });
-    return instance_.get();
-  }
+    Singleton &operator=(const Singleton &) = delete;
 
- protected:
-  // 单例模板类的构造函数和析构函数，私有化以防止外部创建或销毁对象
-  Singleton() = default;
-  ~Singleton() = default;
+    Singleton(Singleton &&) = delete;
 
-  // 单例模板类的成员变量，包括一个智能指针指向单例对象，和一个once_flag变量保证线程安全
-  static std::unique_ptr<T> instance_;
-  static std::once_flag flag_;
+    Singleton &operator=(Singleton &&) = delete;
+
+    // 单例模板类的获取实例函数，使用完美转发和std::call_once来创建并返回单例对象
+    static T *GetInstance(Args &&... args) {
+      std::call_once(flag_, [&]() { instance_ = std::make_unique<T>(std::forward<Args>(args)...); });
+      return instance_.get();
+    }
+
+  protected:
+    // 单例模板类的构造函数和析构函数，私有化以防止外部创建或销毁对象
+    Singleton() = default;
+
+    ~Singleton() = default;
+
+    // 单例模板类的成员变量，包括一个智能指针指向单例对象，和一个once_flag变量保证线程安全
+    static std::unique_ptr<T> instance_;
+    static std::once_flag flag_;
 };
 
 // 初始化单例模板类的静态成员变量
@@ -47,5 +52,11 @@ std::unique_ptr<T> Singleton<T, Args...>::instance_ = nullptr;
 
 template<typename T, typename... Args>
 std::once_flag Singleton<T, Args...>::flag_;
+
+template<typename T, typename... Args>
+T *SingletonOf(Args &&... args) {
+  return Singleton<T, Args...>::GetInstance(std::forward<Args>(args)...);
+}
+}
 
 #endif // SINGLETON_H
